1de2709893045c5b39c591157f53b0f86c3ab467
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-07-27 11:55:34 +0300

    add dynamic data in react component

--- 01. React-Basic-Working-With-Components/01-starting-setup/src/components/ExpenseItem.js
+++ 01. React-Basic-Working-With-Components/01-starting-setup/src/components/ExpenseItem.js
@@ -2,13 +2,18 @@ import './ExpenseItem.css';
 
 // Every React component is function which should be exported/imported 
 function ExpenseItem() {
+	const date = new Date(2021, 8, 8);
+	const title = "Cargo fees";
+	const amount = 294.67;
+
+
 	return (
 		// In the HTML part the component must have always only one root element!!!
 		<div className='expense-item'> {/* react class binding syntax - 'className' */}
-			<div>Date</div>
+			<div>{date.toISOString()}</div> {/* the date object must be cast to string */}
 			<div className='expense-item__description'>
-				<h2>Title</h2>
-				<div className='expense-item__price'>Amount</div>
+				<h2>{title}</h2>{/* react dynamic output syntax - {variable} */}
+				<div className='expense-item__price'>{amount}</div>
 			</div>
 		</div>
 	)
