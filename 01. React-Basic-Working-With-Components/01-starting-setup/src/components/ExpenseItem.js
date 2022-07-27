12784d98bd1e3f01c99ad9302b9a226b76543715
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-07-27 10:52:19 +0300

    add root element to new component

--- 01. React-Basic-Working-With-Components/01-starting-setup/src/components/ExpenseItem.js
+++ 01. React-Basic-Working-With-Components/01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,12 @@
 function ExpenseItem() {
-    return (<h2>ExpenseItem component</h2>)
+	return (
+		// Every React component must have always only one root element!!!
+		<div>
+			<div>Date</div>
+			<h2>Title</h2>
+			<div>Amount</div>
+		</div>
+	)
 }
 
 export default ExpenseItem;
