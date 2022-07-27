3a23702bb6a7a4f62d5940296ef6bda2cd90afa5
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-07-27 12:23:24 +0300

    add parent-child props example

--- 01. React-Basic-Working-With-Components/01-starting-setup/src/App.js
+++ 01. React-Basic-Working-With-Components/01-starting-setup/src/App.js
@@ -1,4 +1,12 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from './components/ExpenseItem/ExpenseItem';
+import ExpenseItemProps from './components/ExpenseItemProps/ExpenseItemProps';
+
+const expenses = [
+  { date: new Date(2021, 8, 8), title: "Expense ratio", amount: 294.67 },
+  { date: new Date(2021, 8, 9), title: "Earnings", amount: 394.67 },
+  { date: new Date(2021, 8, 10), title: "Book value", amount: 494.67 },
+]
+
 
 function App() {
   return (
@@ -6,6 +14,9 @@ function App() {
       <h2>Let's get started!</h2>
       <p>Text paragraph</p>
       <ExpenseItem />
+      <ExpenseItemProps data={expenses[0]} /> {/* passing props to child component */}
+      <ExpenseItemProps data={expenses[1]} /> {/* passing props to child component */}
+      <ExpenseItemProps data={expenses[2]} /> {/* passing props to child component */}
     </div>
   );
 }
---
+++ 01. React-Basic-Working-With-Components/01-starting-setup/src/components/ExpenseItemProps/ExpenseItemProps.js
@@ -0,0 +1,18 @@
+import './ExpenseItemProps.css';
+
+// All passed props from the parent are received in 'props' argument 
+// In case of many props send from the parent - all can be accessed by props such as props.title, props.date, props.amount
+function ExpenseItem(props) {
+
+	return (
+		<div className='expense-item'>
+			<div>{props.data.date.toISOString()}</div>
+			<div className='expense-item__description'>
+				<h2>{props.data.title}</h2>
+				<div className='expense-item__price'>{props.data.amount}</div>
+			</div>
+		</div>
+	)
+}
+
+export default ExpenseItem;
