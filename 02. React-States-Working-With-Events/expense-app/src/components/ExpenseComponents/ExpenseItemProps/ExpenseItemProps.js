76a0aea258c332356e0c52ced06418a041ab595e
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-07-27 17:27:00 +0300

    add useState() and update the component

--- 02. React-States-Working-With-Events/expense-app/src/components/ExpenseComponents/ExpenseItemProps/ExpenseItemProps.js
+++ 02. React-States-Working-With-Events/expense-app/src/components/ExpenseComponents/ExpenseItemProps/ExpenseItemProps.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../../UI/Card/Card';
 import './ExpenseItemProps.css';
@@ -6,13 +7,24 @@ import './ExpenseItemProps.css';
 // In case of many props send from the parent - all can be accessed by props such as props.title, props.date, props.amount
 function ExpenseItem(props) {
 
+	// The function must be called inside the components (there is exception) function in order the data to be reactive
+	// needs default state -> props.data.title
+	// always returns array with two values: 1) is the updated value, 2) function for updating the value
+	const [title, setTitle] = useState(props.data.title);
+
+
+	const clickHandler = () => {
+		setTitle('Updated');
+	}
+
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.data.date} />
 			<div className='expense-item__description'>
-				<h2>{props.data.title}</h2>
+				<h2>{title}</h2>
 				<div className='expense-item__price'>{props.data.amount}</div>
 			</div>
+			<button onClick={clickHandler}>Change title</button>
 		</Card>
 	)
 }
