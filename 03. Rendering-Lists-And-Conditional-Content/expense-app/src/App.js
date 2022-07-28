8c855bc32236953ac93bef2129bd9b4c40df8cb2
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-07-28 17:43:54 +0300

    set init app

---
+++ 03. Rendering-Lists-And-Conditional-Content/expense-app/src/App.js
@@ -0,0 +1,25 @@
+import NewExpense from './components/NewExpense/NewExpense/NewExpense';
+import Expenses from './components/ExpenseComponents/Expenses/Expenses/Expenses';
+
+
+const expenses = [
+  { date: new Date(2021, 8, 8), title: "Expense ratio", amount: 294.67 },
+  { date: new Date(2021, 9, 9), title: "Earnings", amount: 394.67 },
+  { date: new Date(2021, 10, 10), title: "Book value", amount: 494.67 },
+]
+
+const App = () => {
+
+  const addExpenseHandler = (data) => {
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <NewExpense onAdd={addExpenseHandler} />
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
