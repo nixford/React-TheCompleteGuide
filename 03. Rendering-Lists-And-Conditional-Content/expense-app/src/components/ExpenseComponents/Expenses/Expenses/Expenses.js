9d217490f203091310276c6a2a077105b6abd535
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-07-29 11:11:29 +0300

    add examples for conditional rendering

--- 03. Rendering-Lists-And-Conditional-Content/expense-app/src/components/ExpenseComponents/Expenses/Expenses/Expenses.js
+++ 03. Rendering-Lists-And-Conditional-Content/expense-app/src/components/ExpenseComponents/Expenses/Expenses/Expenses.js
@@ -13,13 +13,27 @@ function Expenses(props) {
 
 	const filteredExp = props.expenses.filter((e) => e.date.getFullYear().toString() === year);
 
+	// Third option for conditional rendering:
+	let expContent = <p className='white'>No expenses found.</p>
+	if (filteredExp.length > 0) {
+		expContent = filteredExp.map((expense) => <ExpenseItem key={expense.id} data={expense} />)
+	}
+
 	return (
 		<div>
 			<Card className='expenses'>
 				<ExpensesFilter selectedYear={year} onSelect={selectYearHandler} />
+
 				{/* Making list in React and passing props to child component */}
-				{filteredExp.length === 0 ? <p className='white'>No expenses found.</p> :
-					filteredExp.map((expense) => <ExpenseItem key={expense.id} data={expense} />)}
+				{/* {filteredExp.map((expense) => <ExpenseItem key={expense.id} data={expense} />)} */}
+
+				{/* OPTIONS for Consitional rendering examples in React */}
+				{/* {filteredExp.length === 0 ? <p className='white'>No expenses found.</p> :
+					filteredExp.map((expense) => <ExpenseItem key={expense.id} data={expense} />)} */}
+				{/* {filteredExp.length === 0 && <p className='white'>No expenses found.</p>}
+				{filteredExp.length > 0 && filteredExp.map((expense) => <ExpenseItem key={expense.id} data={expense} />)} */}
+				{expContent}
+
 			</Card>
 		</div>
 
