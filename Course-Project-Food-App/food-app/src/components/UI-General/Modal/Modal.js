c44091f578ec907734eb89cde6390c01d295f575
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-02 16:22:53 +0300

    add initial Modal component

---
+++ Course-Project-Food-App/food-app/src/components/UI-General/Modal/Modal.js
@@ -0,0 +1,13 @@
+import classes from "./Modal.module.css";
+
+const BackDrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
