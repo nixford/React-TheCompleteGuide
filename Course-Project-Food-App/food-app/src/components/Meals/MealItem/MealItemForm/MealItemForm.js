0d2c1aed13b2cc09d4dbc3f3680e37f3a49e2b18
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-02 15:41:16 +0300

    add MealItemForm component

---
+++ Course-Project-Food-App/food-app/src/components/Meals/MealItem/MealItemForm/MealItemForm.js
@@ -0,0 +1,12 @@
+import classess from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classess.form}>
+      <input />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
