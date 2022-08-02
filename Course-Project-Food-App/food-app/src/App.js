8b8af5198e315c62a78261087bd0c3a7d3fec311
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-02 17:07:40 +0300

    add close/open state to Modal component

--- Course-Project-Food-App/food-app/src/App.js
+++ Course-Project-Food-App/food-app/src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Layout/Header/Header";
 import Meals from "./components/Meals/Meals/Meals";
 import Cart from "./components/Cart/Cart/Cart";
 
 function App() {
+  const [cartIsVisible, setCartVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setCartVisible(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartVisible(false);
+  };
+
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {cartIsVisible && <Cart onCloseCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- Course-Project-Food-App/food-app/src/components/Layout/Header/Header.js
+++ Course-Project-Food-App/food-app/src/components/Layout/Header/Header.js
@@ -1,21 +1,21 @@
 import React from "react";
 
 import CartButton from "../CartButton/CartButton";
-import mealsImg from '../../../assets/meals.jpg';
-import classes from './Header.module.css';
+import mealsImg from "../../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <CartButton />
+        <CartButton onActivate={props.onShowCart} />
       </header>
-      <div className={classes['main-image']}>
+      <div className={classes["main-image"]}>
         <img src={mealsImg} alt="Food table" />
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Header;
