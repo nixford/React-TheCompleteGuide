b367acd8908dac08f8f9b9a8213c02a7dd69f39b
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-02 10:18:46 +0300

    add useContext - Context API example

--- 05. Effects-Reducers-Context/test-app-project/src/components/MainHeader/Navigation.js
+++ 05. Effects-Reducers-Context/test-app-project/src/components/MainHeader/Navigation.js
@@ -1,38 +1,66 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+
+  // OPTION 2 - Context API - useContext:
+  const ctx = useContext(AuthContext);
+
   return (
-    // OPTION 1 - "Provider" => "Consumer" components:
-    // Example for Context API - consumer component
-    <AuthContext.Consumer>
-      {(ctx) => { /*ctx => Context API*/
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }
-      }
-    </AuthContext.Consumer>)
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+
+
+
+  // OPTION 1 - Context API - "Provider" => "Consumer" components:
+  // Example for Context API - consumer component
+  // return (   
+  //   <AuthContext.Consumer>
+  //     {(ctx) => { /*ctx => Context API*/
+  //       return (
+  //         <nav className={classes.nav}>
+  //           <ul>
+  //             {ctx.isLoggedIn && (
+  //               <li>
+  //                 <a href="/">Users</a>
+  //               </li>
+  //             )}
+  //             {ctx.isLoggedIn && (
+  //               <li>
+  //                 <a href="/">Admin</a>
+  //               </li>
+  //             )}
+  //             {ctx.isLoggedIn && (
+  //               <li>
+  //                 <button onClick={props.onLogout}>Logout</button>
+  //               </li>
+  //             )}
+  //           </ul>
+  //         </nav>
+  //       );
+  //     }
+  //     }
+  //   </AuthContext.Consumer>)
 
 };
 
