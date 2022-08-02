660e28fb1a73703cf7dbf1434be6ca11b7e872c4
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-02 13:56:11 +0300

    add CartButton and CartIcon

--- Course-Project-Food-App/food-app/src/components/Layout/Header/Header.js
+++ Course-Project-Food-App/food-app/src/components/Layout/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import CartButton from "../CartButton/CartButton";
 import mealsImg from '../../../assets/meals.jpg';
 import classes from './Header.module.css';
 
@@ -8,7 +9,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <CartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImg} alt="Food table" />
