728523df8e1c0a4f533efd0b93ba766986b3bbc0
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-03 13:14:12 +0300

    add button bump when add item

--- Course-Project-Food-App/food-app/src/components/Layout/CartButton/CartButton.js
+++ Course-Project-Food-App/food-app/src/components/Layout/CartButton/CartButton.js
@@ -1,18 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../../Cart/CartIcon/CartIcon";
 import classes from "./CartButton.module.css";
 import CartContext from "../../../store/cart-context";
 
 const CartButton = (props) => {
+  const [isBtnBump, setBtnBump] = useState(false);
   const cartCtx = useContext(CartContext); // The useContext expects CartContext, but not CartProvider
 
   const cartItemsCount = cartCtx.items.reduce((curNum, item) => {
     return curNum + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${isBtnBump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnBump(true);
+
+    const timeIntervalID = setTimeout(() => {
+      setBtnBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeIntervalID);
+    };
+  }, [cartCtx.items]);
+
   return (
-    <button className={classes.button} onClick={props.onActivate}>
+    <button className={btnClasses} onClick={props.onActivate}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
