648bed45b31fe5882bc074540c89c2ea1cd7b04c
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-03 11:31:26 +0300

    add dynamic items in chart and conditional order btn

--- Course-Project-Food-App/food-app/src/components/Cart/Cart/Cart.js
+++ Course-Project-Food-App/food-app/src/components/Cart/Cart/Cart.js
@@ -1,14 +1,19 @@
+import { useContext } from "react";
+import CartContext from "../../../store/cart-context";
+
 import Modal from "../../UI-General/Modal/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed()}`;
+
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[
-        { id: "c1", name: "Sushi", amount: 2, price: 12.99 },
-        { id: "c2", name: "Pizza", amount: 1, price: 10.99 },
-        { id: "c3", name: "Chicken", amount: 1, price: 9.99 },
-      ].map((ci) => (
+      {cartCtx.items.map((ci) => (
         <li>{ci.name}</li>
       ))}
     </ul>
@@ -19,13 +24,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onCloseCart}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
