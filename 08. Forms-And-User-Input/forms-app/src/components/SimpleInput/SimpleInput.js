2942b8630b7b4366edef5403373998a4d075c2c1
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-04 12:15:24 +0300

    initial input validation

--- 08. Forms-And-User-Input/forms-app/src/components/SimpleInput/SimpleInput.js
+++ 08. Forms-And-User-Input/forms-app/src/components/SimpleInput/SimpleInput.js
@@ -3,9 +3,9 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   // useState can be used when we need to check the value on submit
   const nameInputRef = useRef();
-
   // useState can be used when we need to check the value on every keystroke
   const [name, setName] = useState("");
+  const [isNameValid, setIsNameValid] = useState(true);
 
   const nameHandler = (e) => {
     setName(e.target.value);
@@ -14,16 +14,27 @@ const SimpleInput = (props) => {
   const submitHandler = (e) => {
     e.preventDefault();
 
-    console.log("name: ", name);
-    console.log("nameInputRef.current.value: ", nameInputRef.current.value);
+    if (name.trim() === "") {
+      setIsNameValid(false);
+      return;
+    }
+
+    setIsNameValid(true);
+
+    // console.log("name: ", name);
+    // console.log("nameInputRef.current.value: ", nameInputRef.current.value);
 
     // nameInputRef.current.value = ""; => not good practise to directply clead inpuit value
     setName("");
   };
 
+  const nameInputClasses = isNameValid
+    ? "form-control"
+    : "form-control invalid input";
+
   return (
     <form onSubmit={submitHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -33,6 +44,7 @@ const SimpleInput = (props) => {
           value={name}
         />
       </div>
+      {!isNameValid && <p className="error-text">Name must not be empty.</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
