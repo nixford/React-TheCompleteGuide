083383aa80d528c46e212f07f01f75fbff6e2779
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-05 16:51:25 +0300

    setting up the redux store

--- 09. Redux/counter-app/src/index.js
+++ 09. Redux/counter-app/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux"; // Setting up the redux store - similar to React Context API provider
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import store from "./store/index"; // Setting up the redux store - the store needs to be imported
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// Setting up the redux store with the "store" property
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
