f28edc8b13b89042376703780b9886a95082ce73
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-05 17:00:07 +0300

    receiving reactive value from store

--- 09. Redux/counter-app/src/components/Counter/Counter.js
+++ 09. Redux/counter-app/src/components/Counter/Counter.js
@@ -1,12 +1,18 @@
-import classes from './Counter.module.css';
+// Receiving value from redux store
+import { useSelector } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  // Receiving value from redux store - the value is reactive
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
