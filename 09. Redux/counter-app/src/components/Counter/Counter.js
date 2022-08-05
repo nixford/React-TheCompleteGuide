2141d5d2e2ad63d05cb91444c17ea28994688927
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-05 17:12:18 +0300

    add updating store

--- 09. Redux/counter-app/src/components/Counter/Counter.js
+++ 09. Redux/counter-app/src/components/Counter/Counter.js
@@ -1,5 +1,5 @@
 // Receiving value from redux store
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
@@ -7,12 +7,30 @@ const Counter = () => {
   // Receiving value from redux store - the value is reactive
   const counter = useSelector((state) => state.counter);
 
+  // Updating value in redux store
+  // dispatch is a function
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    // dispatch is receiving object with key "type" and value, which is equal as the value in the store
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    // dispatch is receiving object with key "type" and value, which is equal as the value in the store
+    dispatch({ type: "decrement" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
