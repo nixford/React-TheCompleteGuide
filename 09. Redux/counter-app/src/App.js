bea3a25a42e370091a51c815031b42b2e71ad9e2
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-08 12:14:51 +0300

    add Header and Auth components

--- 09. Redux/counter-app/src/App.js
+++ 09. Redux/counter-app/src/App.js
@@ -1,7 +1,16 @@
+import { Fragment } from "react";
+import Header from "./components/Header/Header";
+import Auth from "./components/Auth/Auth";
 import Counter from "./components/Counter/Counter";
 
 function App() {
-  return <Counter />;
+  return (
+    <Fragment>
+      <Header />
+      <Auth />
+      <Counter />
+    </Fragment>
+  );
 }
 
 export default App;
