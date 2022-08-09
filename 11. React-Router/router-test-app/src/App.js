605fdb9f628f8de0ced05804ff0605ed91783359
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-09 14:44:53 +0300

    add basic styling

--- 11. React-Router/router-test-app/src/App.js
+++ 11. React-Router/router-test-app/src/App.js
@@ -1,6 +1,6 @@
 import { Route } from "react-router-dom";
 
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import Welkome from "./views/Welkome";
 import Products from "./views/Products";
 
