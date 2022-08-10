24d9dc22f3f99005e13f0b111cc9c6d7e0756d1a
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-10 16:47:33 +0300

    add programmatic navigation example

--- 11. React-Router/router-test-app/src/views/Products.js
+++ 11. React-Router/router-test-app/src/views/Products.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import { Link } from "react-router-dom";
 
 const Products = () => {
+  const history = useHistory();
+
+  const doneHandler = () => {
+    history.push("/welkome");
+  };
+
   return (
     <section>
       <h1>Products</h1>
@@ -17,6 +24,8 @@ const Products = () => {
           <Link to="/products/Online Course">Online Course</Link>
         </div>
       </section>
+      <br />
+      <button onClick={doneHandler}>Done-programatic-navigation</button>
     </section>
   );
 };
